45f815049c6b9c12ae99209168a65c6b2ab654e8
AriellaYamada <ariella.y.b@hotmail.com>
2015-06-10 23:20:33 -0300

    Fix do merge

--- BookFile.c
+++ BookFile.c
@@ -1,4 +1,5 @@
 #include "BookFile.h"
+#include <string.h>
 
 int createBookFile (FILE *book_file) {
 	long int size;
@@ -20,5 +21,46 @@ int createBookFile (FILE *book_file) {
 	}
 
 	return SUCCESS;
+}
+
+int addBook (FILE *book_file, Book *book_data) {
+
+	if (book_file == NULL)
+		return INVALID_FILE;
+
+	char *reg, aux[20];
+
+	//Escreve o tamanho do registro no arquivo
+	fwrite(&book_data->size, sizeof(int), 1, book_file);
+
+	reg = (char *) malloc (book_data->size * sizeof(char *));
+
+	strcat(reg, book_data->title);
+	strcat(reg, "|");
+	strcat(reg, book_data->author);
+	strcat(reg, "|");
+	strcat(reg, book_data->publisher);
+	strcat(reg, "|");
+	sprintf(aux, "%d", book_data->year);
+	strcat(reg, aux);
+	strcat(reg, "|");
+	strcat(reg, book_data->language);
+	strcat(reg, "|");
+	sprintf(aux, "%d", book_data->pages);
+	strcat(reg, aux);
+	strcat(reg, "|");
+	sprintf(aux, "%f", book_data->price);
+	strcat(reg, aux);
+	strcat(reg, "#");
+
+	fwrite(&reg, book_data->size * sizeof(char), 1, book_file);
+
+	free(reg);
+
+	return SUCCESS;
+}
+
+int searchByYear (FILE *book_file, int *year) {
+	return INVALID_FILE;
 }
 
